5e538c3eb0f8de09b91c4e22adadd78bf790a40c
owen200104@gmail.com <owen200104@gmail.com@3d18feea-5900-11de-992f-fdd7e25b9163>
2009-06-15 01:02:40 +0000

    can add Account(Student), Course , Activity, ActivityGroup to DB now

---
+++ src/Project/Exception/No_Such_Activity.java
@@ -0,0 +1,17 @@
+package Project.Exception;
+
+public class No_Such_Activity extends No_Such_Element {
+
+
+
+	public No_Such_Activity(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public No_Such_Activity(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/Project/Exception/No_Such_ActivityGroup.java
@@ -0,0 +1,16 @@
+package Project.Exception;
+
+public class No_Such_ActivityGroup extends No_Such_Element {
+
+
+	public No_Such_ActivityGroup(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public No_Such_ActivityGroup(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
